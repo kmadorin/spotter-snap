f3bea6e8f1edc8e8505a98042fe70d5b86658556
Kirill Madorin <madorinkv@gmail.com>
2023-08-22 01:01:22 +0200

    deploy script added

--- packages/site/gatsby-config.ts
+++ packages/site/gatsby-config.ts
@@ -3,7 +3,7 @@ import { GatsbyConfig } from 'gatsby';
 const config: GatsbyConfig = {
   // This is required to make use of the React 17+ JSX transform.
   jsxRuntime: 'automatic',
-
+  pathPrefix: "/spotter-snap",
   plugins: [
     'gatsby-plugin-svgr',
     'gatsby-plugin-styled-components',
